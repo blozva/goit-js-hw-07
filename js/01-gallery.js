0862cb08da75bc4d8c38f204282d42861c4c5d82
Ruslana Blozva <runa.blozva@gmail.com>
2022-02-14 11:22:20 +0200

    HW-1(gallery)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,6 +4,35 @@ import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
 
+const gallery  = document.querySelector('div.gallery');
+
+const galleryEl = document.createElement("ul");
+galleryEl.classList.add('gallery');
+
+gallery.append(galleryEl);
+console.log(gallery);
+
+
+
+const pictureElements = galleryItems.map(
+    img => `<li class = "gallery__item"><img src="${img.preview}" srcset="${img.preview} 1x, ${img.original} 2x" alt="${img.description}" class="gallery__image"></li>`
+).join('');
+galleryEl.insertAdjacentHTML("beforeend", pictureElements);
+
+document.querySelectorAll('img.gallery__image').forEach(
+    function (element) {
+        element.addEventListener('click', showPicture);
+    }
+)
+
+function showPicture(event) {
+    const picture = event.target.srcset.split(' ')[2];
+    const instance = basicLightbox.create(`
+        <img src="${picture}" width="100%">
+    `);
+    instance.show();
+}
+
 /*
 Создай галерею с возможностью клика по её элементам и просмотра 
 полноразмерного изображения в модальном окне. Посмотри демо видео работы галереи.
