41b41213c85e23aabeda126f5c7883a7b1447009
Ruslana Blozva <runa.blozva@gmail.com>
2022-02-15 11:14:05 +0200

    gallery, task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery  = document.querySelector('ul.gallery');
+
+console.log(gallery);
+
+
+const pictureElements = galleryItems.map(
+    img => `<li><a class="gallery__item" href="${img.original}">
+    <img class="gallery__image" src="${img.preview}" alt="${img.description}">
+    </a></li>`
+).join('');
+gallery.insertAdjacentHTML("beforeend", pictureElements);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
